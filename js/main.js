646e56f36a9f402ae5ec3278d47cc9ce51ab3776
Robbie Gay <robbiecgay@gmail.com>
2019-10-01 16:38:45 -0400

    Array that tracks board position

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@
 // GLOBAL VARIABLES
 let app = document.getElementById('app');
 let puzzleBoard = renderElement('div', 'row')
-let emptyPos = 10;
+let emptyPos = 3;
+let tilePos = [];
+let winCond = [0, 1, 2, 'X', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 // Function to render elements
 function renderElement(element, classes) {
@@ -25,6 +27,14 @@ function loadPuzzle() {
     let title = renderElement('h1', 'my-5 display-4 text-white');
     title.innerHTML = 'Sliding Puzzle';
 
+    let rand = renderElement('button', 'col-6 button bg-primary');
+    rand.setAttribute('type', 'button');
+    rand.innerHTML = 'RANDOMIZE';
+
+    let upload = renderElement('button', 'col-6 button bg-primary');
+    upload.setAttribute('type', 'button');
+    upload.innerHTML = 'UPLOAD';
+
     let bottom = renderElement('div', 'p-5');
 
     // Append Elements
@@ -32,20 +42,24 @@ function loadPuzzle() {
 
     // Add tiles to the puzzleBoard
     buildBoard(16);
+
     
     centerCol.appendChild(puzzleBoard);
-
+    
+    centerCol.appendChild(rand);
+    centerCol.appendChild(upload);
+    
     centerCol.appendChild(bottom);
-
+    
     row.appendChild(rightCol);
     row.appendChild(centerCol);
     row.appendChild(leftCol);
-
+    
     container.appendChild(row);
-
+    
     
     app.appendChild(container);
-
+    
     buildTiles(16);
 }
 
@@ -63,9 +77,6 @@ class Tile {
     }
     move() {
         alert('You told me to move!');
-    }
-    test(test) {
-        alert(`${test}`);
     }
 }
 
@@ -91,28 +102,53 @@ function buildTiles(size) {
     for (let i = 0; i < size; i++) {
         let puzzlePiece = new Tile(50, i);
         puzzlePiece.draw();
-        // puzzlePiece.test('dsjdsfjsidfjisfd');
+        tilePos.push(i);
     }
     document.getElementById(emptyPos).setAttribute('class', 'col-3 bg-dark display-1 border');
+    tilePos.splice(emptyPos, 1, 'X')
+
 }
 
 function moveTile(e) {
-    console.log(`You clicked on: ${e.target.id}`);
-    console.log(`Empty Square Pos before func: ${emptyPos}`);
+    // console.log(`You clicked on: ${e.target.id}`);
+    // console.log(`Empty Square Pos before func: ${emptyPos}`);
     // Left, Right, Up, Down
     if (e.target.id === `${Number(emptyPos) - 1}` || e.target.id === `${Number(emptyPos) + 1}` || e.target.id === `${Number(emptyPos) - 4}` || e.target.id === `${Number(emptyPos) + 4}`) {
+
         // Changes the clicked tile to Dark
         e.target.setAttribute('class', 'col-3 bg-dark display-1 border');
+        tilePos.splice(e.target.id, 1, 'X')
+
         // Changes the previously dark tile to Not-Dark
         document.getElementById(emptyPos).setAttribute('class', 'col-3 bg-danger display-1 border');
+        if (e.target.id === `${Number(emptyPos) - 1}`) {
+            tilePos.splice(emptyPos, 1, (Number(emptyPos) - 1));
+        }
+        if (e.target.id === `${Number(emptyPos) + 1}`) {
+            tilePos.splice(emptyPos, 1, (Number(emptyPos) + 1));
+        }
+        if (e.target.id === `${Number(emptyPos) - 4}`) {
+            tilePos.splice(emptyPos, 1, (Number(emptyPos) - 4));
+        }
+        if (e.target.id === `${Number(emptyPos) + 4}`) {
+            tilePos.splice(emptyPos, 1, (Number(emptyPos) + 4));
+        }
+
         // Sets the new emptyPos to the new Dark tile
         emptyPos = (e.target.id);
-        console.log(`Empty Square Pos after func: ${emptyPos}`);
-        console.log(`${Number(emptyPos) - 1}`);
-        console.log(`${Number(emptyPos) + 1}`);
-        console.log(`${Number(emptyPos) - 4}`);
-        console.log(`${Number(emptyPos) + 4}`);
+        // console.log(`Empty Square Pos after func: ${emptyPos}`);
+        // console.log(`${Number(emptyPos) - 1}`);
+        // console.log(`${Number(emptyPos) + 1}`);
+        // console.log(`${Number(emptyPos) - 4}`);
+        // console.log(`${Number(emptyPos) + 4}`);
+        console.log(tilePos);
+
+        // Check win conditions
+        if (String(tilePos) === String(winCond)) {
+            alert('You win!!!!');
+        }
     }
+
 }
 
 
@@ -121,6 +157,16 @@ function moveTile(e) {
 
 
 
+// switch (e.target.id) {
+//     case (`${Number(emptyPos) - 1}`):
+//         tilePos.splice(emptyPos, 1, Number(emptyPos - 1));
+//     case (`${Number(emptyPos) + 1}`):
+//         tilePos.splice(emptyPos, 1, Number(emptyPos + 1));
+//     case (`${Number(emptyPos) - 4}`):
+//         tilePos.splice(emptyPos, 1, Number(emptyPos - 4));
+//     case (`${Number(emptyPos) + 4}`):
+//         tilePos.splice(emptyPos, 1, Number(emptyPos + 4));
+// }
 
 
 
@@ -141,7 +187,7 @@ function moveTile(e) {
 //         tileObj.id = `${i + 1}`;
 //         // Why give an object an innerHTML
 //         tileObj.innerHTML = `This is #${i + 1}`
-        
+
 //         // HTML is a property of the tile object?
 //         tileObj.tileDiv = renderElement('div', 'col-3 bg-warning');
 
